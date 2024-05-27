cba8a0635349b39fe74a12548f5d751a65a49541
Michael Avrukin <mavrukin@gmail.com>
2024-05-27 15:02:45 -0400

    solution for chapter 3 exercise 9

---
+++ chapter3/chapter_3_exercise_9.cpp
@@ -0,0 +1,35 @@
+// Chapter 3 Exercise 9 - Calculate the number of mega grain winnings
+#include <iostream>
+
+int main() {
+    int starting_square_grains = 1;
+    constexpr int multiplication_factor = 2;
+    int total_grains_so_far = 0;
+    int grains_for_1000 = -1;
+    int grains_for_1_000_000 = -1;
+    int grains_for_1_000_000_000 = -1;
+
+    for (int i = 1; i <= 64; ++i) {
+        total_grains_so_far += starting_square_grains;
+        if (grains_for_1000 == -1 && total_grains_so_far >= 1000) {
+            grains_for_1000 = i;
+        }
+        if (grains_for_1_000_000 == -1 && total_grains_so_far >= 1000000) {
+            grains_for_1_000_000 = i;
+        }
+        if (grains_for_1_000_000_000 == -1 && total_grains_so_far >= 1000000000) {
+            grains_for_1_000_000_000 = i;
+        }
+        starting_square_grains *= multiplication_factor;
+
+        std::cout << "Current Square: " << i << std::endl;
+        std::cout << "Total Grains So Far: " << total_grains_so_far << std::endl;
+        std::cout << "Current Multiple: " << starting_square_grains << std::endl;
+    }
+
+    std::cout << "Squares for 1,000: " << grains_for_1000 << std::endl;
+    std::cout << "Squares for 1,000,000: " << grains_for_1_000_000 << std::endl;
+    std::cout << "Squares for 1,000,000,000: " << grains_for_1_000_000_000 << std::endl;
+
+    return 0;
+}
